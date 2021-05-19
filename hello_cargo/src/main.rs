2a9d54da2bee25fd937552d0a1a9a44c910dd22e
Vithuran Sadagopan <vithuransoccer1@gmail.com>
2021-05-19 12:55:14 -0400

    minigrep

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
+    let mut x = 5;
+    println!("{}",x);
+    x = 6;
+    println!("{}",x);
+
+   
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("{}", spaces)
+    
+    
+
 }
